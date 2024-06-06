514ccf5e63a8f1ff7a3077786ea0c77fbbee5d5c
Monacada16 <juanxomoncada@gmail.com>
2024-06-06 12:08:16 -0500

    Initial commit for frontend

--- src/config/datebase.js
+++ src/config/datebase.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
-
+MONGO_URI = `mongodb://mongo:zJbgZxTOtYqJojKkpEbqkaVfhMabnPHu@roundhouse.proxy.rlwy.net:34703/`
 const datebase = async ()=>{
     await mongoose.connect(process.env.MONGO_URI);
 };
