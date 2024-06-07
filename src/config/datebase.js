3413ce69ae84678409e0991320c1087c1df5bae6
Monacada16 <juanxomoncada@gmail.com>
2024-06-07 13:53:05 -0500

    backend

--- src/config/datebase.js
+++ src/config/datebase.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
-MONGO_URI = `mongodb://mongo:zJbgZxTOtYqJojKkpEbqkaVfhMabnPHu@roundhouse.proxy.rlwy.net:34703/`
+MONGO_URI = `mongodb+srv://juanxomoncada:bbPEz3uqKMslBj3w@prueba.gatqhha.mongodb.net/Movies`
 const datebase = async ()=>{
     await mongoose.connect(process.env.MONGO_URI);
 };
